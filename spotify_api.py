dd731715d339bd1ef13351b26ad36c62d07be77e
Kenneth Silvestre <ksilv@bu.edu>
2026-07-02 21:20:36 +0000

    feat: add Spotify login endpoint and authorization URL generation

--- spotify_api.py
+++ spotify_api.py
@@ -1,6 +1,9 @@
 import os
 import time
 import requests
+import random
+import string
+import urllib.parse
 from dotenv import load_dotenv
 
 # loading environment variables from .env file
@@ -38,9 +41,36 @@ def get_spotify_config():
         "redirect_url" : redirect_url
     }
 
+# recommended by spotify documentation for security
+def generate_random_string(length=16):
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choices(characters, k=length))
+
+
+# requests user authorization and builds the url that the user would login to
+def build_user_login_url():
+    # Return dictionary of conifguration
+    configuration = get_spotify_config()
+    state = generate_random_string()
+    scope = 'user-read-private user-read-email'
+
+    query_params = {
+        'response_type': 'code',
+        'client_id': configuration["client_id"],
+        'scope': scope,
+        'redirect_url': configuration["redirect_url"],
+        'state': state
+    }
+
+    return 'https://accounts.spotify.com/authorize?' + urllib.parse.urlencode(query_params)
+
 # testing
 if __name__ == "__main__":
     configuration = get_spotify_config()
     print(bool(configuration["client_id"]))
     print(bool(configuration["client_secret"]))
     print(bool(configuration["redirect_url"]))
+
+    print()
+    login_url = build_user_login_url()
+    print(login_url)
