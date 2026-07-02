03c987c33f9e3a001b58ca93549e9868b45ddf94
Kaung San Lin <kaunglin445@gmail.com>
2026-07-02 19:45:34 +0000

    chore: Set up python file and initial plan

---
+++ database.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import sqlalchemy as db
+
+### Database Idea:
+'''
+Since we only have one entry per day, we don't need to have
+two databases for the calendar and the entries
+
+Possible Columns:
+- Date (SQL Datatype)
+- Song Name (VARCHAR)
+- Spotify Link to the Song (VARCHAR)
+- Link to Song/Album Image (VARCHAR)
+- Journal Text (SQL TEXT)
+- Some sort of map API data (TBD)
+- Future: Scrapbooking data
+
+Plan: 
+- Make a SQLite database with one table
+- Try making ORM functions so we can query directly to database
+    without having to write SQL
+'''
