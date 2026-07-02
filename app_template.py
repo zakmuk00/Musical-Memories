6a56454dcf621a19c0067f14113fb565ff12618b
Kaung San Lin <kaunglin445@gmail.com>
2026-07-02 21:52:56 +0000

    feat: working flow from flask app to inserting rows into database

--- app_template.py
+++ app_template.py
@@ -1,6 +1,7 @@
 from database import db
 from entry import Entry
 from flask import Flask
+from datetime import date
 
 def start_app():
     app = Flask(__name__)
@@ -14,4 +15,19 @@ def start_app():
 
 if __name__ == '__main__':
     app = start_app()
-    app.run(debug=True)
+    with app.app_context():
+        sample_entry = Entry(date=date(2026, 7, 2),
+                            song_name='Love, Fig', 
+                            spotify_link='https://open.spotify.com/track/1UybzZ9Iag33eM0maYoqxC',
+                            song_image='https://i.scdn.co/image/ab67616d0000e1a3ae75d2043f5d1a0f9ea7a9ef',
+                            journal_text='I woke up to this song on shuffle, it was a great morning')
+        db.session.add(sample_entry)
+
+        sample_entry_2 = Entry(date=date(2026, 7, 3),
+                            song_name='Gangnam Style', 
+                            spotify_link='https://open.spotify.com/album/0ZjxizLeMyFEjR27JIvD99',
+                            song_image='https://i.scdn.co/image/ab67616d0000e1a36cfc57e5358c5e39e79bccbd',
+                            journal_text='A true classic. Cant get enough of this honestly.')
+        db.session.add(sample_entry_2)
+        db.session.commit()
+    app.run(debug=True, use_reloader=False)
