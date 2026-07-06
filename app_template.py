995126d9e33912a90e0158bd085b67824a8eb00b
Kaung San Lin <kaunglin445@gmail.com>
2026-07-06 19:36:10 +0000

    feat: Implemeted DELETE methods for database

--- app_template.py
+++ app_template.py
@@ -1,5 +1,5 @@
 from database import db
-from entry import Entry, add_entry
+from entry import Entry, add_entry, get_by_id, get_by_date, delete_by_id, delete_by_date, delete_table
 from flask import Flask
 from datetime import date
 
@@ -16,6 +16,7 @@ def start_app():
 if __name__ == '__main__':
     app = start_app()
     with app.app_context():
+        # normal new row
         sample_entry = Entry(date=date(2026, 7, 2),
                             song_name='Love, Fig', 
                             spotify_link='https://open.spotify.com/track/1UybzZ9Iag33eM0maYoqxC',
@@ -25,6 +26,7 @@ if __name__ == '__main__':
         db.session.add(sample_entry)
         db.session.commit()
 
+        # new row with implemented method
         add_entry(date(2026, 7, 3), 
                     'Gangnam Style', 
                     'https://open.spotify.com/album/0ZjxizLeMyFEjR27JIvD99',
@@ -39,4 +41,13 @@ if __name__ == '__main__':
         #                     journal_text='A true classic. Cant get enough of this honestly.',
         #                     location_name='On a walk')
         # db.session.add(sample_entry_2)
+        get_by_id(1)
+        get_by_id(3)
+        get_by_date(7, 3, 2026)
+        delete_by_id(1)
+
+        # delete_table() # Deletes the entire table, might need this to avoid integrity erros while testing
+
+
+        
     app.run(debug=True, use_reloader=False)
--- entry.py
+++ entry.py
@@ -1,5 +1,7 @@
 from database import db
+import datetime
 
+# The ORM model of the entry
 class Entry(db.Model):
     __tablename__ = 'calendar_entries'
     id = db.Column(db.Integer, primary_key=True)
@@ -12,15 +14,92 @@ class Entry(db.Model):
     # latitude = db.Column(db.String(255)) # Optional
     # longitude = db.Column(db.String(255)) # Optional
 
+# Adds a new row into table
+# Only accepts multiple parameters so far
+# Might need another version where an Entry object can be passed
 def add_entry(date, song, link, image, text, location):
     # Validation needed
+    if not type(date) is datetime.date:
+        print('Wrong date format')
+        return
+    
+    if not (type(song) is str and type(link) is str and type(image) is str):
+        print('Invalid song data')
+        return
+    
+    if not type(text) is str:
+        print('Invalid journal data')
+        return
+
+    if not type(location) is str:
+        print('Invalid location data')
+        return
+    
     db.session.add(Entry(date=date, song_name=song, spotify_link=link, song_image=image, journal_text=text, location_name=location))
     db.session.commit()
-    
-def get_by_id(id):
-    # Validation needed
-    return Entry.query.get(id)
 
-def get_by_date(date):
+# Returns Entry object based on the id
+# Only prints results for now, will return object later
+def get_by_id(id):
+    response = Entry.query.get(id)
+    if response is None:
+        print('Id not in table')
+        print()
+    else:
+        print('id:', response.id)
+        print('song name:', response.song_name)
+        print('created:', response.date)
+        print()
+        # return response
+
+# Returns Entry object based on date in the format (mm, dd, yyyy)
+# Parameters are nums and not strings
+# Only prints results for now, will return object later
+def get_by_date(month, day, year):
     # Validation needed
-    return Entry.query.filter_by(date=date).first()
+    query_date = datetime.date(year, month, day)
+    response = Entry.query.filter_by(date=query_date).first()
+    if response is None:
+        print('Date not in table')
+        print()
+    else:
+        print('id:', response.id)
+        print('song name:', response.song_name)
+        print('created:', response.date)
+        print()
+        # return response
+
+# Deletes rows with the given id
+# Returns boolean value of deletion status
+def delete_by_id(id):
+    response = Entry.query.get(id)
+    if response is not None:
+        db.session.delete(response)
+        db.session.commit()
+        print("Delete success")
+        return True
+    else:
+        print("Deletion failed")
+        return False
+
+# Deletes rows with the given date in format (mm, dd, yyyy)
+# Parameters are nums and not strings
+# Returns boolean of deletion status
+def delete_by_date(month, day, year):
+    query_date = datetime.date(year, month, day)
+    response = Entry.query.filter_by(date=query_date).first()
+    if response is not None:
+        db.session.delete(response)
+        db.session.commit()
+        print("Delete success")
+        return True
+    else:
+        print("Deletion failed")
+        return False
+
+# Deletes the entire table
+# Will not be that useful but good to have
+def delete_table():
+    Entry.query.delete()
+    db.session.commit()
+    print('Table deleted')
