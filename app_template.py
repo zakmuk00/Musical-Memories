06107b8152e4df684566b1bc2947cdf5a04c0e41
Kaung San Lin <kaunglin445@gmail.com>
2026-07-07 17:03:35 +0000

    feat: Added user_id as Entry's fields and updated methods accordingly

--- app_template.py
+++ app_template.py
@@ -17,7 +17,8 @@ if __name__ == '__main__':
     app = start_app()
     with app.app_context():
         # normal new row
-        sample_entry = Entry(date=date(2026, 7, 2),
+        sample_entry = Entry(user_id='user1',
+                            date=date(2026, 7, 2),
                             song_name='Love, Fig', 
                             spotify_link='https://open.spotify.com/track/1UybzZ9Iag33eM0maYoqxC',
                             song_image='https://i.scdn.co/image/ab67616d0000e1a3ae75d2043f5d1a0f9ea7a9ef',
@@ -27,7 +28,8 @@ if __name__ == '__main__':
         db.session.commit()
 
         # new row with implemented method
-        add_entry(date(2026, 7, 3), 
+        add_entry('user1',
+                    date(2026, 7, 3), 
                     'Gangnam Style', 
                     'https://open.spotify.com/album/0ZjxizLeMyFEjR27JIvD99',
                     'https://i.scdn.co/image/ab67616d0000e1a36cfc57e5358c5e39e79bccbd',
@@ -41,11 +43,11 @@ if __name__ == '__main__':
         #                     journal_text='A true classic. Cant get enough of this honestly.',
         #                     location_name='On a walk')
         # db.session.add(sample_entry_2)
-        get_by_id(1)
-        get_by_id(3)
-        get_by_date(7, 3, 2026)
-        delete_by_id(1)
-        update_entry(2, location_name='At a throwback party')
+        get_by_id('user1', 1)
+        get_by_id('user1', 3)
+        get_by_date('user1', date(2026, 7, 3))
+        delete_by_id('user1', 1)
+        update_entry('user1', 2, location_name='At a throwback party')
 
         # delete_table() # Deletes the entire table, might need this to avoid integrity erros while testing
 
--- entry.py
+++ entry.py
@@ -3,21 +3,37 @@ import datetime
 
 # The ORM model of the entry
 class Entry(db.Model):
+    """
+    This is the class that defines a journal entry in our app
+    Attributes:
+        id (int): Auto-incremented unique Entry key
+        user_id (str): Identified who made the Entry
+        date (date): The date of the journal Entry
+        song_name (str): Name of the song
+        spotify_link (str): Url to the song on Spotify
+        song_image (str): Url to the song's image (recommend using Spotify api)
+        journal_text (str): Optional journaling text written by user
+        location_name (str): The location the journal entry was created
+    """
     __tablename__ = 'calendar_entries'
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date, unique=True, nullable=False)
+    user_id = db.Column(db.String(255), nullable=False)
+    date = db.Column(db.Date, nullable=False)
     song_name = db.Column(db.String(255), nullable=False)
     spotify_link = db.Column(db.String(255), nullable=False)
     song_image = db.Column(db.String(255), nullable=False)
     journal_text = db.Column(db.Text) # Optional
+    # Need to make a backend route for this
+    # journal_image = db.Column(db.String(255))
     location_name = db.Column(db.String(255), nullable=False)
     # latitude = db.Column(db.String(255)) # Optional
     # longitude = db.Column(db.String(255)) # Optional
+    __table_args__ = (db.UniqueConstraint('user_id', 'date', name='unique_user_date'),)
 
 # Adds a new row into table
 # Only accepts multiple parameters so far
 # Might need another version where an Entry object can be passed
-def add_entry(date, song, link, image, text, location):
+def add_entry(user, date, song, link, image, text, location):
     # Validation needed
     if not type(date) is datetime.date:
         print('Wrong date format')
@@ -35,30 +51,36 @@ def add_entry(date, song, link, image, text, location):
         print('Invalid location data')
         return
     
-    db.session.add(Entry(date=date, song_name=song, spotify_link=link, song_image=image, journal_text=text, location_name=location))
+    db.session.add(Entry(user_id=user, date=date, song_name=song, spotify_link=link, song_image=image, journal_text=text, location_name=location))
     db.session.commit()
 
 # Returns Entry object based on the id
 # Only prints results for now, will return object later
-def get_by_id(id):
+def get_by_id(query_user_id, id):
     response = Entry.query.get(id)
     if response is None:
         print('Id not in table')
         print()
-    else:
-        print('id:', response.id)
-        print('song name:', response.song_name)
-        print('created:', response.date)
+        return None
+    elif response.user_id != query_user_id:
+        print('User is not authorized for access')
         print()
-        # return response
+        return None
+    else:
+        return response
+
+def get_all_by_user(query_user_id):
+    response = Entry.query.filter_by(user_id=query_user_id).all()
+    if response is None:
+        print('No entries by user')
+    return response
 
 # Returns Entry object based on date in the format (mm, dd, yyyy)
 # Parameters are nums and not strings
 # Only prints results for now, will return object later
-def get_by_date(month, day, year):
+def get_by_date(query_user_id, query_date):
     # Validation needed
-    query_date = datetime.date(year, month, day)
-    response = Entry.query.filter_by(date=query_date).first()
+    response = Entry.query.filter_by(user_id=query_user_id, date=query_date).first()
     if response is None:
         print('Date not in table')
         print()
@@ -67,12 +89,12 @@ def get_by_date(month, day, year):
         print('song name:', response.song_name)
         print('created:', response.date)
         print()
-        # return response
+        return response
 
 # Deletes rows with the given id
 # Returns boolean value of deletion status
-def delete_by_id(id):
-    response = Entry.query.get(id)
+def delete_by_id(query_user_id, query_id):
+    response = Entry.query.filter_by(user_id=query_user_id, id=query_id).first()
     if response is not None:
         db.session.delete(response)
         db.session.commit()
@@ -82,12 +104,16 @@ def delete_by_id(id):
         print("Deletion failed")
         return False
 
-# Deletes rows with the given date in format (mm, dd, yyyy)
-# Parameters are nums and not strings
-# Returns boolean of deletion status
-def delete_by_date(month, day, year):
-    query_date = datetime.date(year, month, day)
-    response = Entry.query.filter_by(date=query_date).first()
+def delete_by_date(query_user_id, query_date):
+    """
+    Deletes an Entry based on the date created
+    Args:
+        query_user_id (str): The user_id must match the id in Entry for access
+        query_date (date): The date of the Entry being searched
+    Returns:
+        bool: The status of deletion
+    """
+    response = Entry.query.filter_by(user_id=query_user_id, date=query_date).first()
     if response is not None:
         db.session.delete(response)
         db.session.commit()
@@ -112,8 +138,8 @@ def delete_table():
 # setattr: https://www.w3schools.com/python/ref_func_setattr.asp
 # kwargs: https://www.w3schools.com/python/python_args_kwargs.asp
 # NOT FULLY TESTED YET
-def update_entry(id, **kwargs):
-    entry = Entry.query.get(id)
+def update_entry(query_user_id, id, **kwargs):
+    entry = Entry.query.filter_by(user_id=query_user_id, id=id).first()
     if entry is None:
         print('Could not find entry')
         return False
