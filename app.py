6cee6c32f2f0d9cd0b5719276ec4f3460cc788be
Kenneth Silvestre <ksilv@bu.edu>
2026-07-08 18:24:58 +0000

    feat: added spotify login routing and callback

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request, session, jsonify
 from forms.noteMakerForm import NoteMakerForm
+from spotify_api import SpotifyClient
+from database import db
+from models import SpotifyToken, save_spotify_tokens
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'overly=secure-token-4-testin@'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///models.db'
+
+db.init_app(app)
 
 @app.route("/")
 def home():
@@ -11,8 +17,33 @@ def home():
 
 @app.route("/login/spotify")
 def spotify_login():
-    spotify_auth_url = ""
-    return redirect(spotify_auth_url)
+    spotify = SpotifyClient()
+    login_url = spotify.build_user_login_url()
+    return redirect(login_url)
+
+# spotify sends user to /callback
+@app.route("/callback")
+def spotify_callback():
+    code = request.args.get("code")
+    spotify = SpotifyClient()
+    spotify.exchange_code_for_access_token(code)
+
+    profile = spotify.get_user_profile()
+    user_id = profile.get("account_id")
+
+    # store user_id in flask session
+    session["user_id"] = user_id
+
+    # save user id and tokens into db
+    save_spotify_tokens(user_id, {
+        "access_token": spotify.access_token,
+        "refresh_token": spotify.refresh_token,
+        "expires_at": spotify.expires_at
+    })
+
+    return redirect("/calendar")
+
+
 
 @app.route("/about")
 def about():
@@ -43,4 +74,6 @@ def get_locations():
     return jsonify(locations)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
